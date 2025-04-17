9081864a73a065f748c95975fe5bdc87194facad
dragonmux <git@dragonmux.network>
2025-04-17 20:16:34 +0100

    misc: Cleaned up the build script now the backtrace logic has been removed

--- build.rs
+++ build.rs
@@ -7,26 +7,8 @@
 //! is detect if we are running nightly Rust, and enable backtrace support for errors
 //! if we are.
 
-use rustc_version::{version_meta, Channel};
-
 fn main()
 {
     // Statically link the Visual C runtime on Windows.
     static_vcruntime::metabuild();
-
-    // If detect-backtrace is enabled (default), detect if we're on nightly or not.
-    // If we're on nightly, enable backtraces automatically.
-    if let Some(_val) = std::env::var_os("CARGO_FEATURE_DETECT_BACKTRACE") {
-        match version_meta() {
-            Ok(version_meta) => {
-                if version_meta.channel == Channel::Nightly {
-                    // Tell Cargo to enable backtraces.
-                    println!("cargo:rustc-cfg=feature=\"backtrace\"");
-                }
-            },
-            Err(e) => {
-                println!("cargo:warning=error detecting rustc version: {}", e);
-            }
-        }
-    }
 }
