755754f0bb531abfc9799c648d4949c0761bbfa1
dragonmux <git@dragonmux.network>
2025-04-27 12:12:04 +0100

    firmware_selector: Begun building the documentation display system for the firmware to allow a user to read about and understand a release variant

--- src/firmware_selector.rs
+++ src/firmware_selector.rs
@@ -3,9 +3,10 @@
 // SPDX-FileContributor: Written by Rachel Mant <git@dragonmux.network>
 
 use std::collections::BTreeMap;
+use std::path::PathBuf;
+use std::time::Duration;
 
-use color_eyre::eyre::eyre;
-use color_eyre::eyre::Result;
+use color_eyre::eyre::{eyre, Result};
 use dialoguer::theme::ColorfulTheme;
 use dialoguer::Select;
 
@@ -24,7 +25,7 @@ enum State
     #[default]
     PickFirmware,
     PickAction(usize),
-    ShowDocs(usize),
+    ShowDocs(usize, usize),
     FlashFirmware(usize),
     Cancel,
 }
@@ -62,11 +63,12 @@ impl<'a> FirmwareMultichoice<'a>
         self.state = match self.state {
             State::PickFirmware => self.firmware_selection()?,
             State::PickAction(index) => self.action_selection(index)?,
+            State::ShowDocs(name_index, variant_index) =>
+                self.show_documentation(name_index, variant_index)?,
             // FlashFirmware and Cancel are both terminal actions whereby the FSM is done,
             // so maintain homeostatis for them here.
             State::FlashFirmware(index) => State::FlashFirmware(index),
             State::Cancel => State::Cancel,
-            _ => todo!(),
         };
 
         Ok(())
@@ -95,11 +97,11 @@ impl<'a> FirmwareMultichoice<'a>
         })
     }
 
-    fn action_selection(&self, index: usize) -> Result<State>
+    fn action_selection(&self, name_index: usize) -> Result<State>
     {
         // Convert from a friendly name index into the matching variant download
-        let friendly_name = self.friendly_names[index];
-        let (index, _) = self.variants
+        let friendly_name = self.friendly_names[name_index];
+        let (variant_index, _) = self.variants
             .iter()
             .enumerate()
             .find(|(_, variant)| variant.friendly_name == friendly_name)
@@ -114,12 +116,38 @@ impl<'a> FirmwareMultichoice<'a>
 
         Ok(match selection {
             Some(item) => match item {
-                0 => State::FlashFirmware(index),
-                1 => State::ShowDocs(index),
+                0 => State::FlashFirmware(variant_index),
+                1 => State::ShowDocs(name_index, variant_index),
                 2 => State::PickFirmware,
                 _ => Err(eyre!("Impossible selection for action"))?
             },
             None => State::Cancel,
         })
+    }
+
+    fn show_documentation(&self, name_index: usize, variant_index: usize) -> Result<State>
+    {
+        // Extract which firmware download we're to work with
+        let variant = self.variants[variant_index];
+        // Convert the path compoment of the download URI to a Path
+        let mut docs_path = PathBuf::from(variant.uri.path());
+        // Replace the file extension from ".elf" to ".md"
+        docs_path.set_extension("md");
+        // Convert back into a URI
+        let mut docs_uri = variant.uri.clone();
+        docs_uri.set_path(
+            docs_path.to_str()
+                .expect("Something went terribly wrong building the documentation URI")
+        );
+
+        // Now try and download this documentation file
+        let client = reqwest::blocking::Client::new();
+        let response = client.get(docs_uri)
+            // Use a 2 second timeout so we don't get stuck forever if the user is
+            // having connectivity problems - better to die early and have them retry
+            .timeout(Duration::from_secs(2))
+            .send()?;
+
+        Ok(State::PickAction(name_index))
     }
 }
