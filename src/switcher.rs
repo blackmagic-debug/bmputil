c3c0220d7e5efa46350e457486017bc1d29f4531
P-Storm <pauldeman@gmail.com>
2025-06-05 12:33:49 +0200

    - fixed probeidentity - added tests

--- src/switcher.rs
+++ src/switcher.rs
@@ -9,6 +9,7 @@ use std::path::PathBuf;
 use std::time::Duration;
 
 use clap::ArgMatches;
+use color_eyre::eyre;
 use color_eyre::eyre::{eyre, Result};
 use dialoguer::theme::ColorfulTheme;
 use dialoguer::Select;
@@ -45,18 +46,14 @@ pub fn switch_firmware(matches: &ArgMatches, paths: &ProjectDirs) -> Result<()>
     println!("Probe {} ({}) selected for firmware update", probe.firmware_identity()?, probe.serial_number()?);
 
     // Now extract the probe's identification, and check it's valid
-    let identity = parse_firmware_identity(&probe.firmware_identity()?);
-    let variant = identity.variant()?;
-    // If we don't know what version of firmware is on the probe, presume it's v1.6 for now..
-    // We can't actually know which prior version to v1.6 it actually is but it's very old either way
-    let firmware_version = identity.version.unwrap_or_else(|| "v1.6".into());
+    let identity: ProbeIdentity = probe.firmware_identity()?.into();
 
     // Figure out where the firmware and metadata cache is
     let cache = paths.cache_dir();
 
     // Grab down the metadata index
     let metadata = download_metadata(cache)?;
-    let (release, firmware) = match pick_release(&metadata, &variant, &firmware_version)? {
+    let (release, firmware) = match pick_release(&metadata, identity)? {
         Some(firmware) => firmware,
         None => {
             println!("firmware release selection cancelled, stopping operation");
@@ -90,6 +87,7 @@ fn select_probe(matches: &ArgMatches) -> Result<Option<BmpDevice>>
     let mut devices = results.pop_all()?;
     // Figure out what to do based on the numeber of matching probes
     match devices.len() {
+        0 => Err(eyre!("No probe to select")),
         // If we have just one probe, return that and be done
         1 => Ok(Some(devices.remove(0))),
         // Otherwise, we've got more than one, so ask the user to make a choice
@@ -116,64 +114,104 @@ fn select_probe(matches: &ArgMatches) -> Result<Option<BmpDevice>>
     }
 }
 
-fn parse_firmware_identity(identity: &String) -> ProbeIdentity
-{
-    let mut probe = None;
-    let mut version = None;
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    // BMD product strings are in one ofthe following forms:
-    // Recent: Black Magic Probe v2.0.0-rc2
-    //       : Black Magic Probe (ST-Link v2) v1.10.0-1273-g2b1ce9aee
-    //    Old: Black Magic Probe
-    // From this we want to extract two main things: version (if available), and probe variety
-    // (probe variety meaning alternative platform kind if not a BMP itself)
+    #[test]
+    fn extract_native(){
+        let res: ProbeIdentity = "Black Magic Probe v2.0.0-rc2".to_string().into();
 
-    // Let's start out easy - check to see if the string contains an opening paren (alternate platform)
-    let opening_paren = identity[BMP_PRODUCT_STRING_LENGTH..].find('(');
-    match opening_paren {
-        // If there isn't one, we're dealing with nominally a native probe
-        None => {
-            // Knowing this, let's see if there are enough characters for a version string, and if there are.. extract it
-            if identity.len() > BMP_PRODUCT_STRING_LENGTH {
-                let version_begin = unsafe { identity.rfind(' ').unwrap_unchecked() };
-                version = Some(identity[version_begin + 1..].to_string());
-            }
-            probe = Some("native".into());
-        },
-        Some(opening_paren) => {
-            let closing_paren = identity[opening_paren..].find(')');
-            match closing_paren {
-                None => error!("Product description for device is invalid, found opening '(' but no closing ')'"),
-                Some(closing_paren) => {
-                    // If we did find the closing ')', then see if we've got a version string
-                    let version_begin = identity[closing_paren..].find(' ');
-                    // If we do, then extract whatever's left of the string as the version number
-                    if let Some(version_begin) = version_begin {
-                        version = Some(identity[version_begin..].to_string());
-                    }
-                    // Now we've dealth with the version information, grab everything inside the ()'s as the
-                    // product string for this probe (normalised to lower case)
-                    probe = Some(identity[opening_paren + 1..closing_paren].to_lowercase());
-                }
-            }
-        },
-    };
+        assert_eq!(res.probe, Some("native".into()));
+        assert_eq!(res.version, Some("v2.0.0-rc2".into()));
+    }
 
-    ProbeIdentity { probe, version }
+    #[test]
+    fn extract_old(){
+        let res: ProbeIdentity = "Black Magic Probe".to_string().into();
+
+        assert_eq!(res.probe, Some("native".into()));
+        assert_eq!(res.version, None);
+    }
+
+    #[test]
+    fn extract_st_link(){
+        let res: ProbeIdentity = "Black Magic Probe (ST-Link v2) v1.10.0-1273-g2b1ce9aee".to_string().into();
+
+        assert_eq!(res.probe, Some("st-link v2".into()));
+        assert_eq!(res.version, Some("v1.10.0-1273-g2b1ce9aee".into()));
+    }
+
+    #[test]
+    fn extract_without_closing(){
+        let res: ProbeIdentity = "Black Magic Probe (ST-Link".to_string().into();
+
+        assert_eq!(res.probe, None);
+        assert_eq!(res.version, None);
+    }
 }
 
-fn pick_release<'a>(metadata: &'a Metadata, variant: &Probe, firmware_version: &String) ->
-    Result<Option<(&'a str, &'a Firmware)>>
+impl From<String> for ProbeIdentity{
+    fn from(identity: String) -> Self {
+        let mut probe = None;
+        let mut version = None;
+
+        // BMD product strings are in one ofthe following forms:
+        // Recent: Black Magic Probe v2.0.0-rc2
+        //       : Black Magic Probe (ST-Link v2) v1.10.0-1273-g2b1ce9aee
+        //    Old: Black Magic Probe
+        // From this we want to extract two main things: version (if available), and probe variety
+        // (probe variety meaning alternative platform kind if not a BMP itself)
+
+        // Let's start out easy - check to see if the string contains an opening paren (alternate platform)
+        let opening_paren = identity[BMP_PRODUCT_STRING_LENGTH..].find('(');
+        match opening_paren {
+            // If there isn't one, we're dealing with nominally a native probe
+            None => {
+                // Knowing this, let's see if there are enough characters for a version string, and if there are.. extract it
+                if identity.len() > BMP_PRODUCT_STRING_LENGTH {
+                    let version_begin = identity.rfind(' ').expect("There should be enough chars to find the space");
+                    version = Some(identity[version_begin + 1..].to_string());
+                }
+                probe = Some("native".into());
+            },
+            Some(opening_paren) => {
+                let closing_paren = identity[opening_paren..].find(')');
+                match closing_paren {
+                    None => error!("Product description for device is invalid, found opening '(' but no closing ')'"),
+                    Some(closing_paren) => {
+                        // If we did find the closing ')', then see if we've got a version string
+                        let version_begin = identity[closing_paren..].find(' ');
+                        // If we do, then extract whatever's left of the string as the version number
+                        if let Some(version_begin) = version_begin {
+                            version = Some(identity[closing_paren + version_begin + 1..].to_string());
+                        }
+                        // Now we've dealth with the version information, grab everything inside the ()'s as the
+                        // product string for this probe (normalised to lower case)
+                        probe = Some(identity[BMP_PRODUCT_STRING_LENGTH + opening_paren + 1 ..=closing_paren].to_lowercase());
+                    }
+                }
+            },
+        };
+
+        ProbeIdentity { probe, version }
+    }
+}
+
+fn pick_release(metadata: &Metadata, identity: ProbeIdentity) ->
+    Result<Option<(&str, &Firmware)>>
 {
+    let variant = &identity.variant()?;
+    let firmware_version = identity.version();
+
     // Filter out releases that don't support this probe, and filter out the one the probe is currently running
     // if there is only a single variant in the release (multi-variant releases still need to be shown)
     let releases: Vec<_> = metadata.releases
         .iter()
+        .filter(|&(version, _)| firmware_version != version)
+        .filter(|&(_, release)| release.firmware[variant].variants.len() != 1)
         .filter(
-            |&(version, release)|
-                !(firmware_version == version && release.firmware[variant].variants.len() == 1) &&
-                release.firmware.contains_key(&variant)
-        )
+            |&(_, release)| release.firmware.contains_key(&variant))
         .collect();
 
     let mut items: Vec<_> = releases
@@ -254,32 +292,46 @@ fn download_firmware(variant: &FirmwareDownload, cache_path: &Path) -> Result<Pa
     Ok(cache_file_name)
 }
 
+impl TryFrom<&str> for Probe {
+    type Error = eyre::Report;
+
+    fn try_from(value: &str) -> std::result::Result<Self, Self::Error> {
+        match value {
+            "96b carbon" => Ok(Probe::_96bCarbon),
+            "blackpill-f401cc" => Ok(Probe::BlackpillF401CC),
+            "blackpill-f401ce" => Ok(Probe::BlackpillF401CE),
+            "blackpill-f411ce" => Ok(Probe::BlackpillF411CE),
+            "ctxlink" => Ok(Probe::CtxLink),
+            "f072-if" => Ok(Probe::F072),
+            "f3-if" => Ok(Probe::F3),
+            "f4discovery" => Ok(Probe::F4Discovery),
+            "hydrabus" => Ok(Probe::HydraBus),
+            "launchpad icdi" => Ok(Probe::LaunchpadICDI),
+            "native" => Ok(Probe::Native),
+            "st-link/v2" => Ok(Probe::Stlink),
+            "st-link v3" => Ok(Probe::Stlinkv3),
+            "swlink" => Ok(Probe::Swlink),
+            _ => Err(eyre!("Probe with unknown product string encountered")),
+        }
+    }
+}
+
 impl ProbeIdentity
 {
     fn variant(&self) -> Result<Probe>
     {
         match &self.probe {
-            Some(product) => {
-                let probe = match product.as_str() {
-                    "96b carbon" => Probe::_96bCarbon,
-                    "blackpill-f401cc" => Probe::BlackpillF401CC,
-                    "blackpill-f401ce" => Probe::BlackpillF401CE,
-                    "blackpill-f411ce" => Probe::BlackpillF411CE,
-                    "ctxlink" => Probe::CtxLink,
-                    "f072-if" => Probe::F072,
-                    "f3-if" => Probe::F3,
-                    "f4discovery" => Probe::F4Discovery,
-                    "hydrabus" => Probe::HydraBus,
-                    "launchpad icdi" => Probe::LaunchpadICDI,
-                    "native" => Probe::Native,
-                    "st-link/v2" => Probe::Stlink,
-                    "st-link v3" => Probe::Stlinkv3,
-                    "swlink" => Probe::Swlink,
-                    _ => return Err(eyre!("Probe with unknown product string encountered")),
-                };
-                Ok(probe)
-            },
+            Some(product) => product.as_str().try_into(),
             None => Err(eyre!("Probe has invalid product string")),
+        }
+    }
+
+    fn version(&self) -> &str {
+        // If we don't know what version of firmware is on the probe, presume it's v1.6 for now..
+        // We can't actually know which prior version to v1.6 it actually is but it's very old either way
+        match &self.version {
+            Some(version) => version,
+            None => "v1.6",
         }
     }
 }
