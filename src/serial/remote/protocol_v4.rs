77c3ff00e0bf7412dbb03758edb2fb1fea0ce021
dragonmux <git@dragonmux.network>
2025-06-23 06:12:06 +0100

    serial/remote/protocol_v4: Switched the remote protocol structure from a new-style structure to traditional style so we can add more members sensibly

--- src/serial/remote/protocol_v4.rs
+++ src/serial/remote/protocol_v4.rs
@@ -10,7 +10,13 @@ use crate::serial::bmd_rsp::BmdRspInterface;
 use crate::serial::remote::protocol_v3::RemoteV3;
 use crate::serial::remote::{BmdAdiV5Protocol, BmdJtagProtocol, BmdRemoteProtocol, BmdSwdProtocol, JtagDev};
 
-pub struct RemoteV4(RemoteV3);
+pub struct RemoteV4
+{
+	/// We're a superset of the v3 protocol, this is an instance of that version of the protocol so we
+	/// can access the unchanged machinary from it such as the SWD and JTAG low-level protocol components.
+	/// This version of the protocol defines new high-level protocol components and support commands only.
+	inner_protocol: RemoteV3,
+}
 
 impl From<Arc<Mutex<BmdRspInterface>>> for RemoteV4
 {
@@ -24,7 +30,9 @@ impl RemoteV4
 {
 	pub(crate) fn new(interface: Arc<Mutex<BmdRspInterface>>) -> Self
 	{
-		Self(RemoteV3::new(interface))
+		Self {
+			inner_protocol: RemoteV3::new(interface),
+		}
 	}
 }
 
@@ -32,12 +40,12 @@ impl BmdRemoteProtocol for RemoteV4
 {
 	fn jtag_init(&self) -> Result<Box<dyn BmdJtagProtocol>>
 	{
-		self.0.jtag_init()
+		self.inner_protocol.jtag_init()
 	}
 
 	fn swd_init(&self) -> Result<Box<dyn BmdSwdProtocol>>
 	{
-		self.0.swd_init()
+		self.inner_protocol.swd_init()
 	}
 
 	fn adiv5_init(&self) -> Option<Arc<dyn BmdAdiV5Protocol>>
@@ -57,21 +65,21 @@ impl BmdRemoteProtocol for RemoteV4
 
 	fn add_jtag_dev(&self, dev_index: u32, jtag_dev: &JtagDev)
 	{
-		self.0.add_jtag_dev(dev_index, jtag_dev);
+		self.inner_protocol.add_jtag_dev(dev_index, jtag_dev);
 	}
 
 	fn get_comms_frequency(&self) -> u32
 	{
-		self.0.get_comms_frequency()
+		self.inner_protocol.get_comms_frequency()
 	}
 
 	fn set_comms_frequency(&self, freq: u32) -> bool
 	{
-		self.0.set_comms_frequency(freq)
+		self.inner_protocol.set_comms_frequency(freq)
 	}
 
 	fn target_clk_output_enable(&self, enable: bool)
 	{
-		self.0.target_clk_output_enable(enable);
+		self.inner_protocol.target_clk_output_enable(enable);
 	}
 }
