e7eaa3769512d658347ad7b2299c2e6dc9a451ea
dragonmux <git@dragonmux.network>
2025-12-16 06:00:41 +0000

    firmware_file: Implemented logic for figuring out what kind of file we're working with and dispatching to a file-specific loader

--- src/firmware_file.rs
+++ src/firmware_file.rs
@@ -2,6 +2,12 @@
 // SPDX-FileCopyrightText: 2025 1BitSquared <info@1bitsquared.com>
 // SPDX-FileContributor: Written by Rachel Mant <git@dragonmux.network>
 
+use std::fs::File;
+use std::io::{Read, Seek};
+use std::path::Path;
+
+use color_eyre::eyre::{Context, Result, eyre};
+
 /// File formats that Black Magic Probe firmware can be in.
 pub enum FirmwareFormat
 {
@@ -13,6 +19,119 @@ pub enum FirmwareFormat
 
 	/// Intel HEX. Typical file extensions: `.hex`, `.ihex`.
 	IntelHex,
+}
+
+trait FirmwareStorage
+{
+	fn find_load_address(&self) -> Option<u32>;
+	fn firmware_data(&self) -> &[u8];
+}
+
+struct RawFirmwareFile
+{
+	contents: Vec<u8>,
+}
+
+struct IntelHexFirmwareFile {}
+
+struct ELFFirmwareFile {}
+
+pub struct FirmwareFile
+{
+	inner: Box<dyn FirmwareStorage>,
+}
+
+impl FirmwareFile
+{
+	/// Construct a FirmwareFile from a path to a file
+	pub fn from_path(file_name: &Path) -> Result<Self>
+	{
+		let mut file =
+			File::open(file_name).wrap_err_with(|| eyre!("Failed to read file {} as firmware", file_name.display()))?;
+
+		let mut signature = [0u8; 4];
+		let _ = file.read(&mut signature)?;
+		file.rewind()?;
+
+		let storage: Box<dyn FirmwareStorage> = if &signature == b"\x7fELF" {
+			Box::new(ELFFirmwareFile::from(file))
+		} else if &signature[0..1] == b":" {
+			Box::new(IntelHexFirmwareFile::from(file))
+		} else {
+			Box::new(RawFirmwareFile::from(file)?)
+		};
+
+		Ok(Self {
+			inner: storage,
+		})
+	}
+}
+
+impl RawFirmwareFile
+{
+	fn from(mut file: File) -> Result<Self>
+	{
+		let mut contents = Vec::new();
+		file.read_to_end(&mut contents)?;
+		Ok(Self {
+			contents,
+		})
+	}
+}
+
+impl FirmwareStorage for RawFirmwareFile
+{
+	fn find_load_address(&self) -> Option<u32>
+	{
+		None
+	}
+
+	fn firmware_data(&self) -> &[u8]
+	{
+		&self.contents
+	}
+}
+
+impl IntelHexFirmwareFile
+{
+	fn from(_file: File) -> Self
+	{
+		Self {}
+	}
+}
+
+impl FirmwareStorage for IntelHexFirmwareFile
+{
+	fn find_load_address(&self) -> Option<u32>
+	{
+		None
+	}
+
+	fn firmware_data(&self) -> &[u8]
+	{
+		&[]
+	}
+}
+
+impl ELFFirmwareFile
+{
+	fn from(_file: File) -> Self
+	{
+		Self {}
+	}
+}
+
+impl FirmwareStorage for ELFFirmwareFile
+{
+	fn find_load_address(&self) -> Option<u32>
+	{
+		None
+	}
+
+	fn firmware_data(&self) -> &[u8]
+	{
+		&[]
+	}
 }
 
 impl FirmwareFormat
