6830a1b474a0aca20257c01f05500f3a4996d7fd
Piotr Esden-Tempski <piotr@esden.net>
2026-02-21 09:52:44 -0800

    We moved to codeberg. See README.

--- src/serial/mod.rs
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT OR Apache-2.0
-// SPDX-FileCopyrightText: 2025 1BitSquared <info@1bitsquared.com>
-// SPDX-FileContributor: Written by Rachel Mant <git@dragonmux.network>
-
-pub mod bmd_rsp;
-pub mod gdb_rsp;
-pub mod interface;
-pub mod remote;
